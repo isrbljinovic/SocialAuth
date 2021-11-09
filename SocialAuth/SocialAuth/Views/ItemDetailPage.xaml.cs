8ef20c408bdc1ab498607f592b40221821aa44af
Ivan Srbljinovic <Ivan.Srbljinovic@outlook.com>
2021-11-09 11:57:23 +0100

    Init sln

---
+++ SocialAuth/SocialAuth/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using SocialAuth.ViewModels;
+using Xamarin.Forms;
+
+namespace SocialAuth.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ SocialAuth/SocialAuth/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using SocialAuth.Models;
+using SocialAuth.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SocialAuth.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
