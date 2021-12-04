373e39946f02b51283162b4a9b1b3110a43f4271
Ivan Srbljinovic <ivan.srbljinovic@outlook.com>
2021-12-04 20:46:56 +0100

    Clean start. Facebook login working. MongoDb in progress.

---
+++ SocialAuth/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SocialAuth.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SocialAuth
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new LoginPage();//MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
